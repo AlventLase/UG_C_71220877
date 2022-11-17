09d7f8f08a5ff00302bfa9b9455203b391121c22
AlventLase <alvent.lase@ti.ukdw.ac.id>
2022-11-17 16:37:37 +0700

    menambah file

---
+++ 1_C_71220877.py
@@ -0,0 +1,15 @@
+a = input("Masukkan nama Anda : ")
+b = input("Masukkan mata kuliah : ")
+c = input("Masukkan grup : ")
+d = "Haloo! "
+
+
+#rumus
+e = a.split(" ")
+f = x[len(x)-1]
+g = x[0]
+h = x[len(x)-1]
+i = " ".join(h)
+
+print("Haloo! " (f,i))
+print("Tergabung dalam kelas pada grup" (g,b,c))
